a4033067d30ff3cf6ff6466b4f4e6450efdf5c5f
Si tu m'aimes <1255754523@qq.com>
2017-12-22 00:12:24 +0800

    uwsgi配置

--- manage.py
+++ manage.py
@@ -6,15 +6,18 @@ from prepare import processDB
 from flask_script import Manager, Shell
 
 app=create_app("development")
-if not os.path.exists(app.config["SQLALCHEMY_DATABASE_URI"].lstrip("sqlite:///")):
+
+if not os.path.exists("/"+app.config["SQLALCHEMY_DATABASE_URI"].lstrip("sqlite:///")):
     appContext = app.app_context()
     appContext.push()
     processDB()
     appContext.pop()
 manager=Manager(app)
+
 def make_shell_context():
     return dict(app=app, db=db,Student=Student,Score=Score)
 manager.add_command("shell", Shell(make_context=make_shell_context))
+
 if __name__=="__main__":
-    #app.run("0.0.0.0",port=5000,debug=True)
-    manager.run()
+    app.run("0.0.0.0",port=5000,debug=True)
+    #manager.run()
